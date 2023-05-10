db3160da6bc01ca9972cacaed818b045f62c3c8c
Christian Wansart <c.wansart@ceyoniq.com>
2023-05-10 17:29:21 +0200

    Trigger Action

--- app/src/main/java/gradle/basic/structure/dep/error/App.java
+++ app/src/main/java/gradle/basic/structure/dep/error/App.java
@@ -7,7 +7,7 @@ import org.apache.commons.lang3.StringUtils;
 
 public class App {
     public String getGreeting() {
-        return StringUtils.isBlank(" ");
+        return StringUtils.isBlank("  ");
     }
 
     public static void main(String[] args) {
